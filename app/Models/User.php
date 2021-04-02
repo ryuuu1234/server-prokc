2df6fb3c188f124a359337eab17849c7cdfbec67
ryuuu1234 <pharyyady@gmail.com>
2021-04-02 12:47:34 +0700

    fillable_users

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,12 @@ class User extends Authenticatable implements JWTSubject
         'name',
         'email',
         'password',
+        'avatar',
+        'notelp',
+        'nowhatsapp',
+        'alamat',
+        'provinsi',
+        'kota',
     ];
 
     /**

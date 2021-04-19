d70495f3311ab270160019df630afdf1d02f1371
ryuuu1234 <pharyyady@gmail.com>
2021-04-19 19:34:20 +0700

    mboh-2

---
+++ app/Models/Payment.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+		'number',
+		'transaction_id',
+		'amount',
+		'method',
+		'status',
+		'token',
+		'payloads',
+		'payment_type',
+		'va_number',
+		'vendor_name',
+		'biller_code',
+		'bill_key',
+    ];
+
+    public const PAYMENT_CHANNELS = ['credit_card', 'mandiri_clickpay', 'cimb_clicks',
+	'bca_klikbca', 'bca_klikpay', 'bri_epay', 'echannel', 'permata_va',
+	'bca_va', 'bni_va', 'other_va', 'gopay', 'indomaret',
+	'danamon_online', 'akulaku'];
+
+    public const EXPIRY_DURATION = 2;
+	public const EXPIRY_UNIT = 'days';
+	  
+
+	public const CHALLENGE = 'challenge';
+	public const SUCCESS = 'success';
+	public const SETTLEMENT = 'settlement';
+	public const PENDING = 'pending';
+	public const DENY = 'deny';
+	public const EXPIRE = 'expire';
+	public const CANCEL = 'cancel';
+
+
+	public const PAYMENTCODE = 'PAY';
+
+}
---
+++ database/migrations/2021_04_19_155036_add_payment_token_to_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPaymentTokenToTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->string('payment_token')->after('status')->nullable();
+            $table->string('payment_url')->after('payment_token')->nullable();
+
+            $table->index('payment_token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropColumn('payment_token');
+            $table->dropColumn('payment_url');
+        });
+    }
+}

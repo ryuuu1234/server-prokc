4277a0f81094e2b6b0d729a7a5cb2fd43e5ea523
ryuuu1234 <pharyyady@gmail.com>
2021-05-02 23:49:43 +0700

    now-again

--- app/Http/Controllers/API/KategoriController.php
+++ app/Http/Controllers/API/KategoriController.php
@@ -23,9 +23,9 @@ class KategoriController extends Controller
         $sekarang = date('Y-m-d H:i:s');
 
         $sumJumlah = Lelang::selectRaw('sum(status)')
-        ->whereColumn('kategori', 'lelangs.kategori')
-        ->whereRaw("(berakhir > ?)",[$sekarang])
-        ->getQuery();
+                ->whereColumn('kategori', 'lelangs.kategori')
+                ->whereRaw('berakhir','>',$sekarang)
+                ->getQuery();
 
         // $data = Beban::select('t_beban_biaya.*')
         //         ->with(['pengeluaran' => function($q) use($tgl_awal, $tgl_akhir) {

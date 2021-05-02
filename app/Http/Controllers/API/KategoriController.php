d5513efdddc574b06fc47b73c9aefaa07c31f715
ryuuu1234 <pharyyady@gmail.com>
2021-05-02 23:56:27 +0700

    ok-dicoba

--- app/Http/Controllers/API/KategoriController.php
+++ app/Http/Controllers/API/KategoriController.php
@@ -24,18 +24,18 @@ class KategoriController extends Controller
 
         $sumJumlah = Lelang::selectRaw('sum(status)')
                 ->whereColumn('kategori', 'lelangs.kategori')
-                ->whereRaw('berakhir','>',$sekarang)
+                ->whereRaw('(berakhir > ) ',[$sekarang])
                 ->getQuery();
 
-        // $data = Beban::select('t_beban_biaya.*')
-        //         ->with(['pengeluaran' => function($q) use($tgl_awal, $tgl_akhir) {
-        //        $q->whereRaw("(created_at >= ? AND created_at <= ?)",[$tgl_awal." 00:00:00", $tgl_akhir." 23:59:59"]);
-        //  }])->selectSub($sumJumlah, 'subtotal')->get();
+        $data = Kategori::select('*')
+                ->with(['lelangs' => function($q) use($sekarang) {
+               $q->whereRaw('(berakhir > ) ',[$sekarang]);
+         }])->selectSub($sumJumlah, 'jumlah')->get();
         
         return response()->json([
             'success'=>true,
             'data'=>$categori->all(),
-            'jumlah'=>$sumJumlah
+            'jumlah'=>$data
         ]);
     }
 

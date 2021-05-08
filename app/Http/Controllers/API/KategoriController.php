7133ebe19b45dfdd734307f7b27fb3d78538460d
ryuuu1234 <pharyyady@gmail.com>
2021-05-08 18:50:55 +0700

    kategori salah

--- app/Http/Controllers/API/KategoriController.php
+++ app/Http/Controllers/API/KategoriController.php
@@ -47,7 +47,7 @@ class KategoriController extends Controller
                     $items = $items->where('name', 'LIKE', '%' . request()->q . '%');
                 })
                 ->with(['lelangs' => function($q) use($sekarang) {
-               $q->whereRaw('(berakhir < ?)',[$sekarang]);
+               $q->whereRaw('(berakhir > ?)',[$sekarang]);
          }])->get();
         
         return response()->json([

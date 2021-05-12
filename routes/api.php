4e20a132b954d40dff760b6b529fc09162448e63
ryuuu1234 <pharyyady@gmail.com>
2021-05-12 20:59:44 +0700

    edit-put

--- routes/api.php
+++ routes/api.php
@@ -130,7 +130,7 @@ Route::group(['middleware' => 'jwt.auth'], function() {
 
         // user
         Route::get('/get_users', [UserController::class, 'get_all_with_params']); 
-        Route::post('/kick_back_user/{id}', [UserController::class, 'kick_back_user']); 
+        Route::put('/kick_back_user/{id}', [UserController::class, 'kick_back_user']); 
     });
 
 });
